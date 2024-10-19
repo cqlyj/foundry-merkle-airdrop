7830e6ddf4cd0ee9b39e00364f4dde4016a93b8c
cqlyj <luoyingjie0721@gmail.com>
2024-10-19 13:52:55 +0800

    write a quick test for merkleAirdrop contract

---
+++ test/MerkleAirdropTest.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {AirToken} from "../src/AirToken.sol";
+
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop public airdrop;
+    AirToken public token;
+    bytes32 public ROOT =
+        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 public constant AMOUNT_TO_CLAIM = 25e18;
+    uint256 public constant AMOUNT_TO_TRANSFER = 100e18;
+    bytes32 public proofOne =
+        0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 public proofTwo =
+        0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+    address user;
+    uint256 userPrivateKey;
+
+    function setUp() external {
+        token = new AirToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        (user, userPrivateKey) = makeAddrAndKey("user");
+        token.mint(token.owner(), AMOUNT_TO_TRANSFER);
+        token.transfer(address(airdrop), AMOUNT_TO_TRANSFER);
+    }
+
+    function testUsersCanCliam() external {
+        uint256 startingBalance = token.balanceOf(user);
+
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("startingBalance", startingBalance);
+        console.log("endingBalance", endingBalance);
+        assertEq(endingBalance, startingBalance + AMOUNT_TO_CLAIM);
+    }
+}
